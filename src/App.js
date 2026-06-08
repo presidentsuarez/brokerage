9696db65cd3dc87a27ba23ea94f916f0a6dd613f
Javier Suarez <javier@thesuarezcapital.com>
2026-06-08 17:13:16 +0000

    New favicon + app icon: black/gold ONE badge (favicon.ico, logo192, logo512), cache-bust v4, manifest name

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ const NAV = [
 
 function PrismMark({ size=32 }) {
   return (
-    <img src={`${process.env.PUBLIC_URL}/logo512.png?v=3`} alt="Realty ONE Group Advantage" width={size} height={size}
+    <img src={`${process.env.PUBLIC_URL}/logo512.png?v=4`} alt="Realty ONE Group Advantage" width={size} height={size}
       style={{ width:size, height:size, flexShrink:0, display:"block", borderRadius:size*0.22, objectFit:"cover" }} />
   );
 }
